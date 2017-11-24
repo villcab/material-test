62bbb389385db63b41d2e889a0e71fe00458414b
Bismarck Villca Soliz <bvillca@vulcan.bo>
2017-11-24 14:19:16 -0400

    Base project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './page/button/button.component';
+import { SlidercComponent } from './page/sliderc/sliderc.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'buttons', pathMatch: 'full' },
+  { path: 'buttons', component: ButtonComponent },
+  { path: 'sliders', component: SlidercComponent }
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(routes, { useHash: true }),
+    MatButtonModule,
+    MatIconModule
+  ],
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    SlidercComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page/sliderc/sliderc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-sliderc',
+  templateUrl: './sliderc.component.html',
+  styleUrls: ['./sliderc.component.scss']
+})
+export class SlidercComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
